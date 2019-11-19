9ee438aa401190504710fbf08a714e827f7738dc
Annan <alberto.annan@accenture.com>
2019-11-19 17:31:15 -0300

    instalaciones sprint 3 epic 1

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,23 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app'
+import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import rootReducer from "./store/reducers/rootReducer";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//import App from './app'
+//ReactDOM.render(<App />, document.getElementById('root'));
 
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+  );
+  
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
