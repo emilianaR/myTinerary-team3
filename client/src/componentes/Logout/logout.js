5ed35f886a5cf479649a4d4ec4b27085cbdf7fb5
Annan <alberto.annan@accenture.com>
2019-12-02 10:28:28 -0300

    version final logout

--- client/src/componentes/Logout/logout.js
+++ client/src/componentes/Logout/logout.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-// import Button from "react-bootstrap/Button";
+import Button from "react-bootstrap/Button";
 // import Form from "react-bootstrap/Form";
 import NavBar from "../Nav/nav";
-// import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 // import "./login.css";
 import { GETEXIT } from '../../store/actions/sesionActions';
 import { connect } from "react-redux";
@@ -15,45 +15,9 @@ class Logout extends Component {
     redirect: false
   };
 
-  /* valueUser(e) {
-    this.setState({ user: e });
+   componentDidMount() {
+    this.props.GETEXIT()
   }
-
-  componentDidUpdate(prevProps) {
-    console.log(this.props.success)
-    if (this.props.success !== prevProps.success){
-        console.log(this.props.success);
-        console.log(prevProps.success);
-        
-      this.setState({
-        redirect: this.props.success
-      });
-    }
- }
-
-  valuePassword(e) {
-    this.setState({ password: e });
-  }
-
-  obtieneLogin(e) {
-    let user = {
-      email: this.state.user,
-      password: this.state.password
-    };
-    console.log('obtieneLogin');
-    this.props.login(user);
-  }
-  renderRedirect = () => {
-    if (this.state.redirect === true) {
-      console.log(this.state.redirect)
-      return <Redirect to='/' />
-    }
-  } */
-
-  componentDidMount() {
-this.props.GETEXIT()
-  }
-
 
   render() {
     return (
@@ -61,27 +25,18 @@ this.props.GETEXIT()
         <NavBar />
         <div className="containerItinerary" style={{ paddingTop: "20px", paddingBottom: "20px" }}>
           <h3 className="row justify-content-center">Logout</h3>
+          <p> gracias por navegar por myTinerary</p>
+        </div>
 
-          <p> gracias por navegar por myTinerary</p>            
-            
-       </div>
+        <div className="btn w-50 m-auto d-block">
+          <Link to="/">
+            <Button alt="button2" > Volver a Home </Button>
+          </Link>
+        </div>
+
       </>
     );
   }
 }
 
-/* const mapStateToProps = (state) => {
-  return {
-    success: state.sesionReducer.success,
-    token: state.sesionReducer.token
-  }
-};
-*/
-
-// const mapDispatchToProps = (dispatch) => ({
-//   login: (user) => {
-//     dispatch(GETEXIT())
-//   }
-// }); 
-
-export default connect(null, {GETEXIT})(Logout);
+export default connect(null, { GETEXIT })(Logout);
